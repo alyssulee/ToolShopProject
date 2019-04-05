e5bc75db68a62d4ddf4e55c157deaa6f87345d12
Owen Troke-Billard <owen.trokebillard@outlook.com>
2019-04-05 12:16:06 -0600

    Document response types

--- src/toolShop/communication/responses/Response.java
+++ src/toolShop/communication/responses/Response.java
@@ -2,8 +2,16 @@ package toolShop.communication.responses;
 
 import toolShop.communication.Message;
 
+/**
+ * An abstract type from which all responses inherit.
+ */
 public abstract class Response extends Message
 {
+    /**
+     * Creates a response with the supplied discriminator.
+     *
+     * @param discriminator The discriminator used to identity the response type.
+     */
     Response(int discriminator)
     {
         super(discriminator);
--- src/toolShop/communication/responses/ToolResponse.java
+++ src/toolShop/communication/responses/ToolResponse.java
@@ -3,23 +3,44 @@ package toolShop.communication.responses;
 import com.sun.istack.internal.Nullable;
 import toolShop.models.Tool;
 
+/**
+ * A response containing a tool.
+ */
 public class ToolResponse extends Response
 {
+    /**
+     * The tool; may be null.
+     */
     @Nullable
     private Tool tool;
 
+    /**
+     * Creates a response containing a tool.
+     *
+     * @param tool The tool; may be null.
+     */
     public ToolResponse(@Nullable Tool tool)
     {
         super(201);
         this.tool = tool;
     }
 
+    /**
+     * Gets the tool; may be null.
+     *
+     * @return The tool; may be null.
+     */
     @Nullable
     public Tool getTool()
     {
         return tool;
     }
 
+    /**
+     * Sets the tool.
+     *
+     * @param tool The tool; may be null.
+     */
     public void setTool(@Nullable Tool tool)
     {
         this.tool = tool;
